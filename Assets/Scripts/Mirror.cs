61e9cdc7c23fa54e66e09cc173fc5ac0979d7b87
Sean Hale <seanhale1@live.com>
2018-01-27 01:41:54 -0500

    dfgh

--- Assets/Scripts/Mirror.cs
+++ Assets/Scripts/Mirror.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class Mirror : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public enum Color { White, Red, Green, Blue };
+    public enum MirrorRot { Rot0, Rot90, Rot180, Rot270 };
+
+    public MirrorRot mRotation = MirrorRot.Rot0;
+    public Color mColor = Color.White;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void onTriggerEnter(Collider other)
+    {
+
+    }
 }
