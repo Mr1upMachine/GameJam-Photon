eace8a9e914c789cd8a565c3eaf9421f30e8eb29
Parz1val1 <timmyhawkins3@gmail.com>
2018-01-27 10:24:35 -0500

    tags & box

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class player : MonoBehaviour {
+public class Player : MonoBehaviour {
 
 	private List<GameObject> lasers = new List<GameObject>();
 	private float shotSpeed;
