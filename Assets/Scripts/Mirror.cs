c28b2ae2ad7648d78d50195ac6cae690a4554ad1
Sean Hale <seanhale1@live.com>
2018-01-27 09:04:50 -0500

    1234

--- Assets/Scripts/Mirror.cs
+++ Assets/Scripts/Mirror.cs
@@ -5,9 +5,6 @@ using UnityEngine;
 public class Mirror : MonoBehaviour {
 
     public enum Color { White, Red, Green, Blue };
-    const int MRot0 = 0, MRot90 = 90, MRot180 = 180, MRot270 = 270;
-
-    public int mRotation = 270;
     public Color mColor = Color.White;
 
     // Use this for initialization
@@ -22,31 +19,25 @@ public class Mirror : MonoBehaviour {
 
     }
 
-	void OnTriggerEnter2D(Collider2D other)
+    private void FixedUpdate()
     {
-		if (other.gameObject.CompareTag("LaserWhite") ||
-			other.gameObject.CompareTag("LaserRed") ||
-			other.gameObject.CompareTag("LaserGreen") ||
-			other.gameObject.CompareTag("LaserBlue"))
-		{
-			
-            
-            
+        
+    }
 
-            if (mRotation == MRot0)
-			{
-                other.gameObject.transform.rotation = Quaternion.Euler(0, 0, -90);
-            }
-            else if (mRotation == MRot270)
-			{
-                other.gameObject.transform.rotation = Quaternion.Euler(0, 0, 90);
-            }
-			else if (mRotation == MRot180 || mRotation == MRot90)
-			{
-                Destroy(other.gameObject);
-			}
-			
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (gameObject.transform.rotation == Quaternion.Euler(0, 0, 0))
+        {
+            other.gameObject.transform.rotation = Quaternion.Euler(0, 0, -90);
+        }
+        else if (gameObject.transform.rotation == Quaternion.Euler(0, 0, 270))
+        {
+            other.gameObject.transform.rotation = Quaternion.Euler(0, 0, 90);
+        }
+        else
+        {
+            Destroy(other.gameObject);
+        }
 
-		}
     }
 }
