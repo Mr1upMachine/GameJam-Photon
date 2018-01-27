df1ae0d81a56e75206b665735a9b23c15801353a
lmh96 <loganhill23@gmail.com>
2018-01-26 23:03:32 -0500

    Merge branch 'master' of https://github.com/Mr1upMachine/GameJam2018
    # Conflicts:
    #	Assets/Plugins.meta

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -4,14 +4,34 @@ using UnityEngine;
 
 public class player : MonoBehaviour {
 
-    private List<GameObject> lasers = new List<GameObject>();
+	private List<GameObject> lasers = new List<GameObject>();
+	private float shotSpeed;
+	public GameObject laserPrefab;
 	// Use this for initialization
 	void Start () {
-		
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+
+		shotSpeed = 5;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetKeyDown("mouse 0"))
+		{
+			GameObject bullet = (GameObject)Instantiate(laserPrefab, transform.position, Quaternion.identity);
+			lasers.Add(bullet);
+		}
+		for(int count = 0; count < lasers.Count;count++ )
+		{
+			if (lasers[count] != null)
+			{
+				lasers[count].transform.Translate(new Vector3(0, 1) * Time.deltaTime * shotSpeed);
+			}
+		}
 	}
 }
