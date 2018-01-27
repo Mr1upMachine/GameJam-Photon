6abe4ab27d45da1e42ce99af77bcb99175f8bede
Sean Hale <seanhale1@live.com>
2018-01-27 10:04:25 -0500

    fixed rotate issue

--- Assets/Scripts/Mirror.cs
+++ Assets/Scripts/Mirror.cs
@@ -117,6 +117,22 @@ public class Mirror : MonoBehaviour {
 
     private void OnMouseDown()
     {
-        transform.Rotate(0, 0, 90);
+        if (transform.rotation == Quaternion.Euler(0, 0, 0))
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 270);
+        }
+        else if (transform.rotation == Quaternion.Euler(0, 0, 270))
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 180);
+        }
+        else if (transform.rotation == Quaternion.Euler(0, 0, 180))
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 90);
+        }
+        else if (transform.rotation == Quaternion.Euler(0, 0, 90))
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+
     }
 }
