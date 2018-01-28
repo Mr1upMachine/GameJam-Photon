c04ca16ca95635b61216961079e1f5bcf45d0386
Sean Hale <seanhale1@live.com>
2018-01-27 22:23:53 -0500

    Revert "Merge branch 'AssetCreate'"
    This reverts commit fefd8f7c89fa98fd82759b8790aca8529ff30786, reversing
    changes made to 8c03080eeaf5d0939330ee7a249ccc093809f31f.

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -2,30 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class player : MonoBehaviour {
 
-	public static List<GameObject> lasers = new List<GameObject>();
+	private List<GameObject> lasers = new List<GameObject>();
 	private float shotSpeed;
 	public GameObject laserPrefab;
-    public Transform spawn;
-    public bool canRotate = true;
-
-    // Use this for initialization
-    void Start () {
-        shotSpeed = 10;
+	// Use this for initialization
+	void Start () {
+        shotSpeed = 5;
 	}
     
 
 	// Update is called once per frame
 	void Update()
 	{
-
-		if (Input.GetKeyDown("space"))
+		if (Input.GetKeyDown("mouse 0"))
 		{
-			GameObject bullet = (GameObject)Instantiate(laserPrefab, spawn.position, spawn.rotation);
+			GameObject bullet = (GameObject)Instantiate(laserPrefab, transform.position, Quaternion.identity);
 			lasers.Add(bullet);
 		}
-		for(int count = 0; count < lasers.Count; count++ )
+		for(int count = 0; count < lasers.Count;count++ )
 		{
 			if (lasers[count] != null)
 			{
@@ -33,29 +29,4 @@ public class Player : MonoBehaviour {
 			}
 		}
 	}
-    private void OnMouseOver()
-    {
-        if (canRotate)
-        {
-            if (Input.GetMouseButtonDown(1))
-            {
-                if (transform.rotation == Quaternion.Euler(0, 0, 0))
-                {
-                    transform.rotation = Quaternion.Euler(0, 0, 270);
-                }
-                else if (transform.rotation == Quaternion.Euler(0, 0, 270))
-                {
-                    transform.rotation = Quaternion.Euler(0, 0, 180);
-                }
-                else if (transform.rotation == Quaternion.Euler(0, 0, 180))
-                {
-                    transform.rotation = Quaternion.Euler(0, 0, 90);
-                }
-                else if (transform.rotation == Quaternion.Euler(0, 0, 90))
-                {
-                    transform.rotation = Quaternion.Euler(0, 0, 0);
-                }
-            }
-        }
-    }
 }
